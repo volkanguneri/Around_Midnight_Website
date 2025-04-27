190f0690bd7ec531f3a59e2b525b579976aabea0
volkanguneri <guneriv@gmail.com>
2025-04-27 17:40:06 +0200

    Fix build configuration and static files copying

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: 'index.html',
-			precompress: false,
 			strict: false
 		}),
 		paths: {
@@ -17,7 +14,7 @@ const config = {
 		},
 		prerender: {
 			handleHttpError: 'warn',
-			default: true
+			entries: ['*']
 		}
 	}
 };
