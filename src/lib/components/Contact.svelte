8ce1044a4a52ff77944e737efa4a847b79c4d306
volkanguneri <guneriv@gmail.com>
2025-04-27 21:35:09 +0200

    Update contact email from personal to professional address

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -27,7 +27,7 @@
 				<div class="rounded-lg p-8">
 					<div class="space-y-6 text-center text-lg md:text-3xl">
 						<p>+33 6 58 78 25 61</p>
-						<p>juliette-chevalier35@hotmail.fr</p>
+						<p>trioaroundmidnight@gmail.com</p>
 						<div class="flex justify-center space-x-6 pt-6">
 							<a
 								href="https://www.instagram.com/around.midnight.trio/"
