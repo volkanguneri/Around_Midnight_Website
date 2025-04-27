b491e6d61d66c95fbd7e4a7977c42da837ebe4d3
volkanguneri <guneriv@gmail.com>
2025-04-27 17:43:35 +0200

    Add YouTube channel link

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -65,9 +65,12 @@
 									/>
 								</svg>
 							</a>
-							<span
-								class="text-white opacity-50"
-								aria-label="YouTube (à venir)"
+							<a
+								href="https://m.youtube.com/channel/UCUTsInt7Vr-stI9aoBnV0Yw"
+								class="text-white transition-transform hover:scale-110 hover:opacity-80"
+								target="_blank"
+								rel="noopener noreferrer"
+								aria-label="YouTube"
 							>
 								<svg
 									class="h-6 w-6 md:h-8 md:w-8"
@@ -79,7 +82,7 @@
 										d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
 									/>
 								</svg>
-							</span>
+							</a>
 						</div>
 					</div>
 				</div>
