05247b3acd8caa077cedde8928236a95e52c0bcd
volkanguneri <guneriv@gmail.com>
2025-04-27 15:22:26 +0200

    Configure GitHub Pages deployment with static adapter and workflow setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			strict: false
+		}),
+		paths: {
+			base: '/Around_Midnight_Website'
+		},
+		prerender: {
+			handleHttpError: 'warn'
+		}
+	}
 };
 
 export default config;
