3f25e2aa92b36219c54bcff0cf3478eccb24f4d7
volkanguneri <guneriv@gmail.com>
2025-04-27 21:27:11 +0200

    Adjust responsive layout and spacing for banner and section components

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <section class="relative" id="banner">
-	<div class="relative w-full bg-black md:h-screen">
+	<div class="relative h-[80vh] w-full bg-black md:h-screen">
 		<img
 			src={getOptimizedImagePath('/images/main.jpg')}
 			alt="Around Midnight Banner"
@@ -24,8 +24,8 @@
 		<div class="absolute inset-0 flex items-center justify-center">
 			<div class="mx-auto max-w-4xl px-4">
 				<div class="space-y-4 text-center md:space-y-4">
-					<h1 class="font-moon text-4xl md:text-6xl">{t('banner.title')}</h1>
-					<p class="text-lg md:text-xl lg:text-2xl">{t('banner.subtitle')}</p>
+					<h1 class="font-moon text-3xl sm:text-4xl md:text-6xl">{t('banner.title')}</h1>
+					<p class="text-base sm:text-lg md:text-xl lg:text-2xl">{t('banner.subtitle')}</p>
 				</div>
 			</div>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,19 +9,19 @@
 
 <div class="relative min-h-screen">
 	<Banner />
-	<section id="trio" class="mb-4 mt-10 scroll-mt-16 md:my-16">
+	<section id="trio" class="mb-12 mt-10 scroll-mt-16 md:my-16">
 		<Trio />
 	</section>
-	<section id="musicians" class="scroll-mt-16 md:mb-8">
+	<section id="musicians" class="scroll-mt-16 md:mb-16">
 		<Musicians />
 	</section>
-	<section id="gallery" class="mb-20 scroll-mt-16 md:mb-20">
+	<section id="gallery" class="mb-20 scroll-mt-16">
 		<Gallery />
 	</section>
-	<section id="videos" class="mb-4 scroll-mt-16 md:mb-20">
+	<section id="videos" class="mb-12 scroll-mt-16 md:mb-20">
 		<Video />
 	</section>
-	<section id="contact" class="scroll-mt-16">
+	<section id="contact" class="mb-12 scroll-mt-16 md:mb-16">
 		<Contact />
 	</section>
 </div>
